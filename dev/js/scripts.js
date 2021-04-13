698b05c59cda88b9bf72de67efb3c4ac4a11e1a3
khuongbhan <hbk32@drexel.edu>
2021-04-13 14:56:51 +0700

    square

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -23,19 +23,22 @@ mainTL
         .from("#orange-circle", {duration: 1, x: 600, delay: 0.5,ease: "bounce.out"})
         .to("#orange-circle", {x:0,duration: .3})
     
-    // Circle jumping up
-    .to("#orange-circle", {delay:1, duration:1, x:-50})
-    .to("#orange-circle", {duration:.1, x:30})
-    .to("#orange-circle", {duration:1.5, y:-200, x:300, ease:'circ'})
+    // Circle jumping out of frame
+        .to("#orange-circle", {delay:1, duration:.3, x:-50})
+        .to("#orange-circle", {duration:.05, x:30})
+        .to("#orange-circle", {duration:.4, y:-200, x:300})
     // .to("#orange-circle", {duration:.5, y:-180, x:330})
     // .to("#orange-circle", {duration:.5, y:-200, x:350})
-    .to("#orange-circle", {duration:1.25, y:0, x:600, ease:'circ'})
-    .to("#orange-circle", {duration:1.5, y:-100, x:850, ease:'circ'})
-    .to("#orange-circle", {duration:1, y:0, x:1150, ease:'circ'})
-    // .to("#orange-circle", {duration:.2, scaleX:1, scaleY: .6, y:0, x:600})
-    // .to("#orange-circle", {duration:.1, scaleY: .2})
-    // .to("#orange-circle", {duration:.1, scaleY: .5})
-    // .to("#orange-circle", {duration:.1, scaleY: 1})
+        .to("#orange-circle", {duration:.3, y:0, x:600})
+        .to("#orange-circle", {duration:.4, y:-100, x:850})
+        .to("#orange-circle", {duration:.2, y:0, x:1150})
+
+    // Circle sprinting back, square appears
+        .to("#orange-circle",{duration: 3, x:-300, y: -200, scaleX: .45, scaleY: .45,ease: "bounce.out"},"squareOut")
+        .from("#green-square",{duration: 1, y:-500, scaleX: 0.3, scaleY: 0.3, rotate: 270},"squareOut")
+        .to("#green-square",{duration: 1, y:0, rotate: 0, scaleX: 1, scaleY: 1})
+        .to("#orange-circle",{duration: .75, x:400, y: 0, scaleX: 1, scaleY: 1,ease: "bounce.out"})
+
 
 
 
